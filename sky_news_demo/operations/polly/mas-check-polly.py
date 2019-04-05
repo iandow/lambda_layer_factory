b865cedcd9fe139776fa03fa754fa2c5915383e8
Dold <brandold@88e9fe70a797.ant.amazon.com>
2019-04-05 15:28:58 -0700

    normalized polly operators (exception handling / output object)

--- sky_news_demo/operations/polly/mas-check-polly.py
+++ sky_news_demo/operations/polly/mas-check-polly.py
@@ -1,44 +1,64 @@
 import boto3
+from outputHelper import OutputHelper
+from outputHelper import MasExecutionError
 
-polly = boto3.client('polly')
-s3 = boto3.client('s3')
+polly = boto3.client("polly")
+s3 = boto3.client("s3")
+
+operator_name = "polly"
+output_object = OutputHelper(operator_name)
 
 def lambda_handler(event, context):
 
     print("We got this event:\n", event)
 
-    task_id = event['metadata']['polly_job_id']
+    try:
+        task_id = event["metadata"]["polly_job_id"]
+    except KeyError:
+        output_object.update_status("Error")
+        output_object.update_metadata(translate_error="No valid job id")
+        raise MasExecutionError(output_object.return_output_object())
+    try:
+        polly_response = polly.get_speech_synthesis_task(
+            TaskId=task_id
+        )
+    except Exception as e:
+        output_object.update_status("Error")
+        output_object.update_metadata(polly_error="Unable to get response from polly: {e}".format(e=str(e)))
+        raise MasExecutionError(output_object.return_output_object())
+    else:
+        polly_status = polly_response["SynthesisTask"]["TaskStatus"]
+        print("The status from polly is:\n", polly_status)
+        if polly_status == "inProgress":
+            polly_job_id = polly_response["SynthesisTask"]["TaskId"]
+            output_object.update_metadata(polly_job_id=polly_job_id)
+            output_object.update_status("Executing")
+            return output_object.return_output_object()
+        elif polly_status == "completed":
+            uri = polly_response["SynthesisTask"]["OutputUri"]
+            file = uri.split("/")[5]
+            folder = uri.split("/")[4]
+            bucket = uri.split("/")[3]
+            key = folder + "/" + file
 
-    # Add error handling
+            output_object.update_metadata(polly_job_id=task_id)
+            output_object.update_media("audio", bucket, key)
+            output_object.update_status("Complete")
+            return output_object.return_output_object()
 
-    polly_response = polly.get_speech_synthesis_task(
-        TaskId=task_id
-    )
+        elif polly_status == "scheduled":
+            polly_job_id = polly_response["SynthesisTask"]["TaskId"]
+            output_object.update_metadata(polly_job_id=polly_job_id)
+            output_object.update_status("Executing")
+            return output_object.return_output_object()
 
-    polly_status = polly_response['SynthesisTask']['TaskStatus']
+        elif polly_status == "failed":
+            output_object.update_status("Error")
+            output_object.update_metadata(polly_error="Polly returned as failed: {e}".format(e=str(polly_response["SynthesisTask"]["TaskStatusReason"])))
+            raise MasExecutionError(output_object.return_output_object())
+        else:
+            output_object.update_status("Error")
+            output_object.update_metadata(polly_error="Polly returned as failed: {e}".format(e=str(polly_response["SynthesisTask"]["TaskStatusReason"])))
+            raise MasExecutionError(output_object.return_output_object())
 
-    print('The status from polly is:\n', polly_status)
 
-    if polly_status == 'inProgress':
-        output = {"name": "polly", "status": "Executing", "metadata": {"polly_job_id": task_id, "bucket": event["metadata"]["bucket"]} }
-        return output
-    if polly_status == 'scheduled':
-        output = {"name": "polly", "status": "Executing", "metadata": {"polly_job_id": task_id, "bucket": event["metadata"]["bucket"] } }
-        return output
-    if polly_status == 'failed':
-        output = {"name": "polly", "status": "Error", "metadata": {"polly_job_id": task_id, "bucket": event["metadata"]["bucket"] } }
-        return output
-    if polly_status == 'completed':
-        uri = polly_response['SynthesisTask']['OutputUri']
-
-        file = uri.split("/")[5]
-        folder = uri.split("/")[4]
-        bucket = uri.split("/")[3]
-
-        key = folder + "/" + file
-
-        output = {"name": "polly", "status": "Complete", "media": {"audio": {"s3bucket": bucket, "s3key": key}}, "metadata": {"polly_job_id": task_id} }
-
-        return output
-
-    # Add else statement here to handle any weirdness
