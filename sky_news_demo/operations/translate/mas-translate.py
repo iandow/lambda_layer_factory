1dbdf75af12cc88c3edbc4d50e653e4363bb1715
Dold <brandold@88e9fe70a797.ant.amazon.com>
2019-04-05 11:35:49 -0700

    normalized translate operator (exception handling / output object)

--- sky_news_demo/operations/translate/mas-translate.py
+++ sky_news_demo/operations/translate/mas-translate.py
@@ -1,35 +1,54 @@
 import boto3
 
+from outputHelper import OutputHelper
+from outputHelper import MasExecutionError
 
 translate_client = boto3.client('translate')
 s3 = boto3.client('s3')
 
+operator_name = "translate"
+output_object = OutputHelper(operator_name)
+
 
 def lambda_handler(event, context):
-    # move these to pull from event
+        try:
+            bucket = event["input"]["media"]["text"]["s3bucket"]
+            key = event["input"]["media"]["text"]["s3key"]
+        except KeyError:
+            output_object.update_status("Error")
+            output_object.update_metadata(translate_error="No valid inputs")
+            raise MasExecutionError(output_object.return_output_object())
 
-    bucket = event["input"]["media"]["text"]["s3bucket"]
-    key = event["input"]["media"]["text"]["s3key"]
+        try:
+            source_lang = event["configuration"]["translate"]["SourceLanguageCode"]
+            target_lang = event["configuration"]["translate"]["TargetLanguageCode"]
+        except KeyError:
+            output_object.update_status("Error")
+            output_object.update_metadata(translate_error="Language codes are not defined")
+            raise MasExecutionError(output_object.return_output_object())
 
-    source_lang = event["configuration"]["translate"]["SourceLanguageCode"]
-    target_lang = event["configuration"]["translate"]["TargetLanguageCode"]
+        try:
+            s3_response = s3.get_object(Bucket=bucket, Key=key)
+            transcript = s3_response["Body"].read().decode("utf-8")
+            print(transcript)
+        except Exception as e:
+            output_object.update_status("Error")
+            output_object.update_metadata(translate_error="Unable to read transcription from S3: {e}".format(e=str(e)))
+            raise MasExecutionError(output_object.return_output_object())
 
-    s3_response = s3.get_object(Bucket=bucket, Key=key)
-    transcript = s3_response["Body"].read().decode("utf-8")
-
-    print(transcript)
-
-    # Need to add error handling
-
-    translation = translate_client.translate_text(
-        Text=transcript,
-        SourceLanguageCode=source_lang,
-        TargetLanguageCode=target_lang
-    )
-
-    translated_transcript = translation["TranslatedText"]
-
-    output = {"name": "translate", "metadata": {"translation": translated_transcript, "bucket": bucket} }
-
-    return output
+        try:
+            translation = translate_client.translate_text(
+                Text=transcript,
+                SourceLanguageCode=source_lang,
+                TargetLanguageCode=target_lang
+            )
+        except Exception as e:
+            output_object.update_status("Error")
+            output_object.update_metadata(translate_error="Unable to get response from translate: {e}".format(e=str(e)))
+            raise MasExecutionError(output_object.return_output_object())
+        else:
+            translated_transcript = translation["TranslatedText"]
+            output_object.update_metadata(translation=translated_transcript)
+            output_object.update_status("Complete")
+            return output_object.return_output_object()
 
